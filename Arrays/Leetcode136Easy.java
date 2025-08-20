0f3b1ddc89a12deef948db6eccffe0eab14deca0
Jayasriramsankarasetty <144556602+Jayasriramsankarasetty@users.noreply.github.com>
2025-08-20 15:02:47 +0530

    Questions on Arrays

---
+++ Arrays/Leetcode136Easy.java
@@ -0,0 +1,9 @@
+class Leetcode136Easy {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
